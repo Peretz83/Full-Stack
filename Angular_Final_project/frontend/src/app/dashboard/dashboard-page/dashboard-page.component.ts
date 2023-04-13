10bc34c0f67b18ab8b07ec7ae8dd01b3cf9d5ad3
Peretz83 <liavperetz83@gmail.com>
2023-04-13 19:21:16 +0300

    first commit

--- Angular_Final_project/frontend/src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ Angular_Final_project/frontend/src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/api.service';
+import { Customer } from 'src/app/shared/interfaces/customerInterface';
+import { Employee } from 'src/app/shared/interfaces/employeeInterface';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.css']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit {
+   customers: Array<Customer> = [];
+   employees: Array<Customer> = [];
+  ngOnInit(): void {
+    this.getAllCustomers()
+    this.getAllEmployees()
+    
+}
+ getAllCustomers() {
+        this.api.getCustomers().subscribe({
+            next: (data: Array<Customer>) => this.customers = data,
+            error: (err) => console.log(err)
+        })
+    }
+    getAllEmployees() {
+        this.api.getEmployees().subscribe({
+            next: (data: Array<Employee>) => this.employees = data,
+            error: (err) => console.log(err)
+        })
+    }
   
+  constructor(private api:ApiService){
+
+  }
+ 
+  today =  Date.now();
+
   title = 'Dashboard'
   icon = 'bi bi-graph-up'
+  title2 = 'Customer'
+  icon2 = 'bi bi-plus-circle-fill'
+
+
+ 
 
 
 }
--- Angular_Final_project/frontend/src/app/dashboard/dashboard.module.ts
+++ Angular_Final_project/frontend/src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
+import { RouterModule } from '@angular/router';
+
+
 
 
 
@@ -11,7 +15,10 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    RouterModule,
+    
   ]
 })
 export class DashboardModule { }
