89e066cca0422a0de4159641ab92348592f9cb9b
Peretz83 <liavperetz83@gmail.com>
2023-04-13 09:30:16 +0300

    Auth+Customers+employees

--- Angular_Final_project1/frontend/src/app/shared/constants/urls.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = 'http://localhost:3002'
-
-export const USERS_URL = '/api/users'
-export const USER_REGISTER_URL = '/api/users/register'
-export const USER_LOGIN_URL = '/api/users/login'
--- Angular_Final_project1/frontend/src/app/shared/interfaces.ts/userInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    _id?: string | null;
-    name?: string | null;
-    email?: string | null;
-    password?: string | null;
-    confirm_password?: string | null;
-    token?: string | null;
-}
